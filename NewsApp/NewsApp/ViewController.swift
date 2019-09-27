d921b7acf2d6de6aea25f01bd574df35fe1f23b6
Anuj Choubey <anujch93@gmail.com>
2019-09-27 08:05:50 +0530

    created a news app

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Kumar Choubey, Anuj on 27/09/19.
+//  Copyright © 2019 Kumar Choubey, Anuj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
